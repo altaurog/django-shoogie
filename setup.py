53fba180955ce1bc77e1455564b3e64a1bf90df6
Aryeh Leib Taurog <git@aryehleib.com>
2012-03-15 22:22:48 +0200

    assign __version__, clean up setup.py

--- setup.py
+++ setup.py
@@ -4,24 +4,5 @@ setup(
     version = "0.1",
     packages = find_packages(),
 
-    # Project uses reStructuredText, so ensure that the docutils get
-    # installed or upgraded on the target machine
     install_requires = ['django>=1.3'],
-
-#   package_data = {
-#       # If any package contains *.txt or *.rst files, include them:
-#       '': ['*.txt', '*.rst'],
-#       # And include any *.msg files found in the 'hello' package, too:
-#       'hello': ['*.msg'],
-#   },
-
-#   # metadata for upload to PyPI
-#   author = "Me",
-#   author_email = "me@example.com",
-#   description = "This is an Example Package",
-#   license = "PSF",
-#   keywords = "hello world example examples",
-#   url = "http://example.com/HelloWorld/",   # project home page, if any
-
-#   # could also include long_description, download_url, classifiers, etc.
 )
