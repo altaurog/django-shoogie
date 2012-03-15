249807a7924631cf7a12092a098c354a5606bf89
Aryeh Leib Taurog <git@aryehleib.com>
2012-03-15 22:54:22 +0200

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "django-shoogie",
-    version = "0.1",
+    version = "0.1.1",
     packages = find_packages(),
 
     install_requires = ['django>=1.3'],
