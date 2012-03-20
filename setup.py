24381515ede0c758acfe5e0d45ffbf0d81722f64
Aryeh Leib Taurog <git@aryehleib.com>
2012-03-20 23:16:51 +0200

    Bump version to 0.2, add meta data

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 setup(
     name = "django-shoogie",
-    version = "0.1.1",
-    packages = find_packages(),
-
+    version = "0.2",
+    description = "Log server errors to database",
+    author = "Aryeh Leib Taurog",
+    author_email = "python@aryehleib.com",
+    url = "http://bitbucket.org/altaurog/django-shoogie",
+    packages = ['shoogie'],
     install_requires = ['django>=1.3'],
 )
