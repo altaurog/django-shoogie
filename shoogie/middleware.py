1bc7c31fdb287f0bbc4ccaab542ae981d047da95
Aryeh Leib Taurog <git@aryehleib.com>
2013-10-20 16:19:22 +0200

    Add SHOOGIE_IGNORE_EXCEPTIONS setting, ignore PermissionDenied by default.

--- shoogie/middleware.py
+++ shoogie/middleware.py
@@ -1,18 +1,38 @@
 import sys
 import traceback
+import warnings
 
 from django         import http
 from django.conf    import settings
+from django.core    import exceptions
 from django.views   import debug
 
 from shoogie        import models
 
+DEFAULT_IGNORES = (http.Http404, exceptions.PermissionDenied)
+
 class ExceptionLoggingMiddleware(object):
+    def __init__(self):
+        ignores = getattr(settings, 'SHOOGIE_IGNORE_EXCEPTIONS', None)
+        if ignores:
+            self.ignores = []
+            for exception_path in ignores:
+                try:
+                    mod, exc = exception_path.rsplit('.', 1)
+                    __import__(mod)
+                    ignore_mod = sys.modules[mod]
+                    ignore_exc = getattr(ignore_mod, exc)
+                    self.ignores.append(ignore_exc)
+                except (ValueError, ImportError, AttributeError):
+                    warnings.warn("Unable to import exception: %s" % exception_path)
+        else:
+            self.ignores = DEFAULT_IGNORES
+
     def process_exception(self, request, exception):
         if settings.DEBUG:
             return
         exc_type, exc_val, tb = sys.exc_info()
-        if issubclass(exc_type, http.Http404):
+        if issubclass(exc_type, self.ignores):
             return
         reporter = debug.ExceptionReporter(request, exc_type, exc_val, tb)
         user     = request.user
