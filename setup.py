51a187875745e2f564479ca2e47aba23962614f5
Aryeh Leib Taurog <git@aryehleib.com>
2013-10-23 21:00:14 +0200

    Preparing for distribution

--- setup.py
+++ setup.py
@@ -1,11 +1,33 @@
+from os.path import join, dirname
 from setuptools import setup
+
+def read(filename):
+    f = open(join(dirname(__file__), filename))
+    return f.read()
+
+
 setup(
     name = "django-shoogie",
     version = "0.4",
     description = "Log server errors to database",
+    long_description = read("README.rst"),
     author = "Aryeh Leib Taurog",
     author_email = "python@aryehleib.com",
     url = "http://bitbucket.org/altaurog/django-shoogie",
-    packages = ['shoogie'],
-    install_requires = ['django>=1.3'],
+    packages = ["shoogie"],
+    install_requires = ["django>=1.3"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Environment :: Web Environment",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: System :: Logging",
+        "Topic :: Database",
+        "Framework :: Django",
+    ],
 )
